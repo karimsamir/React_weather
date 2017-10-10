8cc6267ab40cb82966ec1890e678db0cd0a3eac6
karimsamir <karim5977@gmail.com>
2017-10-10 21:31:44 +0200

    57 Ajax requests with Axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = "1513a1fa0d9c85cc87a27b1115cd7026";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = "FETCH_WEATHER";
+//http://samples.openweathermap.org/data/2.5/forecast?q=M%C3%BCnchen,DE&appid=b1b15e88fa797225412429c1c50c122a1
+
+export function fetchWeather(city){
+
+  const url = `${ROOT_URL}&q=${city},eg`;
+  const request = axios.get(url);
+
+  return{
+    type: FETCH_WEATHER
+  }
+}
