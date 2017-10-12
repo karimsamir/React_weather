46411223eabb60d62042d1467ee1a3f533fc6bcd
karimsamir <karim5977@gmail.com>
2017-10-12 13:04:04 +0200

    65 Making a Reusable Chart Component

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,22 +1,21 @@
 import React, { Component} from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
-
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
 
   renderWeather(cityData){
     const name = cityData.city.name;
     const temps = cityData.list.map(weather =>   weather.main.temp);
+    const pressures = cityData.list.map(weather =>   weather.main.pressure);
+    const humidities = cityData.list.map(weather =>   weather.main.humidity);
     console.log(temps);
       return(
         <tr key={name}>
           <td>{name}</td>
-          <td>
-            <Sparklines height={40} width={60} data={temps}>
-              <SparklinesLine color="red" />
-            </Sparklines>
-          </td>
+          <td><Chart data={temps} color="orange" units="K" /></td>
+          <td><Chart data={pressures} color="green" units="hPa" /></td>
+          <td><Chart data={humidities} color="black" units="%" /></td>
         </tr>
       );
     }
@@ -27,9 +26,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>city</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
